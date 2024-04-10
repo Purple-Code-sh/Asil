fc86410b2e72b86d94f576fbb40abbe2c2368f65
Purple-Code-sh <purple.code.sh@gmail.com>
2024-04-10 15:42:09 -0600

    changes all movility functions due to errors with Xmotion board

--- Asil minisumo/src/main.cpp
+++ Asil minisumo/src/main.cpp
@@ -15,8 +15,8 @@ int front_L = 1;
 int side_R = 4;
 // int front_R = 4;
 
-int motorL_pwm = 3;
-int motorL_dir = 12;
+int motorL_pwm = 12;
+int motorL_dir = 3;
 
 int motorR_pwm = 11;
 int motorR_dir = 13;
@@ -26,122 +26,9 @@ int v_Front;
 int v_Side;
 
 int proportionalUsed;
+int counter;
 
 Servo FLAG;
-
-void setup()
-{
-  Serial.begin(9600);
-  FLAG.attach(servo_B);
-
-  // pinMode(startPin, INPUT);
-  //   pinMode(line_L, INPUT);
-  //  pinMode(line_R, INPUT);
-  pinMode(side_L, INPUT);
-  pinMode(side_R, INPUT);
-  pinMode(front_L, INPUT);
-  pinMode(front_R, INPUT);
-
-  pinMode(motorL_pwm, OUTPUT);
-  pinMode(motorL_dir, OUTPUT);
-  pinMode(motorR_pwm, OUTPUT);
-  pinMode(motorR_dir, OUTPUT);
-  pinMode(servo_B, OUTPUT);
-
-  // v_Line = 0;
-  v_Front = 0;
-  v_Side = 0;
-
-  proportionalUsed = 0;
-
-  FLAG.write(80);
-
-  goForward(255, 255, 3000);
-  delay(5000);
-}
-
-void loop()
-{
-start:
-  // if (digitalRead(startPin) == HIGH) {
-  // FLAG.write(190);
-
-  // ---------- Line ----------
-  // if (analogRead(line_I) < 600)
-  // {
-  //   Serial.println("Linea - izq");
-  //   goBack(50, 255, 200);
-  //   goRight(false, 255, 90, 1);
-  //   goto start;
-  // }
-
-  // ---------- Front ----------
-
-  v_Front = frontSensors();
-  if (v_Front == 3)
-  {
-    if (proportionalUsed == 0)
-    {
-      Serial.println("Enfrente - ambos");
-      // goForward_proportional(80);
-      proportionalUsed = 1;
-      goto start;
-    }
-    else if (proportionalUsed == 1)
-    {
-      // goForward(255, 255, 30);
-      goto start;
-    }
-  }
-  else if (v_Front == 1)
-  {
-    Serial.println("Enfrente - izquierda");
-    goLeft(false, 70, 20, 1);
-    proportionalUsed = 0;
-    goto start;
-  }
-  else if (v_Front == 2)
-  {
-    Serial.println("Enfrente - derecha");
-    goRight(false, 70, 20, 1);
-    proportionalUsed = 0;
-    goto start;
-  }
-
-  // ---------- Side ----------
-
-  v_Side = sideSensors();
-
-  if (v_Side == 1)
-  {
-    Serial.println("Lado - izquierda");
-    goFull_left();
-    proportionalUsed = 0;
-    goto start;
-  }
-  else if (v_Side == 2)
-  {
-    Serial.println("Lado - derecha");
-    goFull_right();
-    proportionalUsed = 0;
-    goto start;
-  }
-  else
-  {
-    Serial.println("Ninguno");
-    //        goLeft_proportional(10);
-    //        delay(150);
-    proportionalUsed = 0;
-    goto start;
-  }
-  // } else {
-  //   Serial.println("Apagado");
-  //   stopi(false);
-  //   proportionalUsed = 0;
-  //   FLAG.write(90);
-  // }
-
-} // <<<---loop end
 
 // ------------------------- Sensors Functions ------------------------- .
 
@@ -213,22 +100,31 @@ void goBack(int powerL_b, int powerR_b, int workTime_b)
   stopi(true);
 }
 
+void goFull_back()
+{
+  digitalWrite(motorL_dir, HIGH);
+  digitalWrite(motorL_pwm, LOW);
+
+  digitalWrite(motorR_dir, HIGH);
+  analogWrite(motorR_pwm, 255);
+}
+
 void goFull_left()
 {
   digitalWrite(motorL_dir, HIGH);
-  analogWrite(motorL_pwm, 90);
+  analogWrite(motorL_pwm, 255);
 
   digitalWrite(motorR_dir, LOW);
-  analogWrite(motorR_pwm, 90);
+  analogWrite(motorR_pwm, 255);
 }
 
 void goFull_right()
 {
-  digitalWrite(motorL_dir, LOW);
-  analogWrite(motorL_pwm, 200);
+  digitalWrite(motorL_dir, HIGH);
+  analogWrite(motorL_pwm, 255);
 
   digitalWrite(motorR_dir, HIGH);
-  analogWrite(motorR_pwm, 200);
+  analogWrite(motorR_pwm, 255);
 }
 
 // ------------------------- Proportional Functions ------------------------- .
@@ -299,3 +195,131 @@ void goLeft(bool curve_l, int power_l, int workTime_l, int relation_l)
     stopi(false);
   }
 }
+
+void setup()
+{
+  Serial.begin(9600);
+  FLAG.attach(servo_B);
+
+  // pinMode(startPin, INPUT);
+  //   pinMode(line_L, INPUT);
+  //  pinMode(line_R, INPUT);
+  pinMode(side_L, INPUT);
+  pinMode(side_R, INPUT);
+  pinMode(front_L, INPUT);
+  pinMode(front_R, INPUT);
+
+  pinMode(motorL_pwm, OUTPUT);
+  pinMode(motorL_dir, OUTPUT);
+  pinMode(motorR_pwm, OUTPUT);
+  pinMode(motorR_dir, OUTPUT);
+  pinMode(servo_B, OUTPUT);
+
+  // v_Line = 0;
+  v_Front = 0;
+  v_Side = 0;
+
+  proportionalUsed = 0;
+  counter = 0;
+
+  FLAG.write(80);
+
+  stopi(true);
+}
+
+void loop()
+{
+start:
+  // if (digitalRead(startPin) == HIGH) {
+  // FLAG.write(190);
+
+  // ---------- Line ----------
+  // if (analogRead(line_I) < 600)
+  // {
+  //   Serial.println("Linea - izq");
+  //   goBack(50, 255, 200);
+  //   goRight(false, 255, 90, 1);
+  //   goto start;
+  // }
+
+  // ---------- Front ----------
+
+  v_Front = frontSensors();
+  if (v_Front == 3)
+  {
+    if (proportionalUsed == 0)
+    {
+      Serial.println("Enfrente - ambos");
+      // goForward_proportional(80);
+      goFull_left();
+      proportionalUsed = 1;
+      goto start;
+    }
+    else if (proportionalUsed == 1)
+    {
+      // goForward(255, 255, 30);
+      goFull_left();
+      goto start;
+    }
+  }
+  else if (v_Front == 1)
+  {
+    Serial.println("Enfrente - izquierda"); // todo izquierda
+    goLeft(false, 70, 10, 1);
+    proportionalUsed = 0;
+    goto start;
+  }
+  else if (v_Front == 2)
+  {
+    Serial.println("Enfrente - derecha");
+    goFull_right();
+    proportionalUsed = 0;
+    goto start;
+  }
+
+  // ---------- Side ----------
+
+  v_Side = sideSensors();
+
+  if (v_Side == 1)
+  {
+    Serial.println("Lado - izquierda"); // pa adelante
+    // goFull_left();
+    goLeft(false, 120, 10, 1);
+    proportionalUsed = 0;
+    goto start;
+  }
+  else if (v_Side == 2)
+  {
+    Serial.println("Lado - derecha");
+    goFull_right();
+    proportionalUsed = 0;
+    goto start;
+  }
+  else
+  {
+    // Serial.println("Ninguno");
+    proportionalUsed = 0;
+    if (counter == 20000)
+    {
+      goFull_left();
+      delay(7);
+      stopi(true);
+      counter = 0;
+    }
+    else
+    {
+      stopi(true);
+      counter++;
+      Serial.println(counter);
+    }
+    goto start;
+  }
+  // } else {
+  //   Serial.println("Apagado");
+  //   stopi(false);
+  //   proportionalUsed = 0;
+  //   FLAG.write(90);
+  // }
+
+} // <<<---loop end
